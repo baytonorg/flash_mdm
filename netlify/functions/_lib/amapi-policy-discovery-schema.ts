8cb889bdb556e43d27b77288e763821f2e0c96e8
Jason Bayton <jason@bayton.org>
2026-09-20 09:09:24 +0100

    chore: refresh AMAPI discovery revision

--- netlify/functions/_lib/amapi-policy-discovery-schema.ts
+++ netlify/functions/_lib/amapi-policy-discovery-schema.ts
@@ -2,7 +2,7 @@
 // Refresh deliberately with `npm run amapi-schema:update` and review the schema diff.
 export const AMAPI_POLICY_DISCOVERY_SCHEMA = {
   "source": "https://androidmanagement.googleapis.com/$discovery/rest?version=v1",
-  "revision": "20260916",
+  "revision": "20260917",
   "root": "Policy",
   "schemas": {
     "AdvancedSecurityOverrides": {
